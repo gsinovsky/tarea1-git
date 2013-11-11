7f2d8f3ca249e2db13dd7f91a02d9483ee70a3a2
gsinovsky <g.sinovsky@gmail.com>
2013-11-11 14:47:43 -0430

    Optimized the code a little bit

--- code/Scrabble_norep.py
+++ code/Scrabble_norep.py
@@ -5,31 +5,32 @@
 
 import urllib
 import string
-import sys
 
 #Alphabet as base comparison
 alphabet = string.ascii_uppercase
 
 #Asks whether it should download or not the list of words
-option = raw_input('Do you have the sowpods.txt file locally? (y/n) ')
-if option == 'y':
-    pass
-elif option == 'n':
-    print 'Downloading file...'
-    urllib.urlretrieve('http://norvig.com/ngrams/sowpods.txt', 'sowpods.txt')
-    print 'File downloaded'
-else:
-    print 'Not a valid answer'
-    sys.exit(0)
+
+b = False
+while b is False:
+    option = raw_input('Do you have the sowpods.txt file locally? (y/n) ')
+    if option == 'y':
+        b = True
+    elif option == 'n':
+        print 'Downloading file...'
+        urllib.urlretrieve('http://norvig.com/ngrams/sowpods.txt', 'sowpods.txt')
+        print 'File downloaded'
+        b = True
+    else:
+        print 'Not a valid answer'
 
 #Proceeds to open the file and checks for repeated letters in every word
 with open('sowpods.txt', 'r') as the_file:
     for word in the_file:
-        for pos, letter in enumerate(word):
-            if pos < len(word) - 1:
-                if letter == word[pos + 1]:
+        for i in range(len(word) - 1):
+            if word[i] == word[i + 1]:
                     #if the letter is repeated, remove from our alphabet
-                    alphabet = alphabet.replace(letter, "")
-print alphabet
+                    alphabet = alphabet.replace(word[i], "")
+print 'The letters are ' + alphabet
 the_file.close()
 #End of program
